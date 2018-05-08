8140cb80ca9e8f80dfe80e978fcf97ea98f1acd5
Yanyuxxxx <812363941@qq.com>
2018-05-08 22:59:00 +0800

    1

---
+++ PythonCook/PythonCook/3.10 1.10 删除序列相同元素并保持顺序.py
@@ -0,0 +1,13 @@
+def dedupe (items):
+    seen = set()
+    for item in items:
+        if item not in seen:
+            yield item
+            seen.add(item)
+
+a = [1, 5, 2, 1, 9, 1, 5, 10]
+g = dedupe(a)
+print(list(g))
+
+# 如果你仅仅就是想消除重复元素，通常可以简单的构造一个集合, 然而，这种方法不能维护元素的顺序，生成的结果中的元素位置被打乱
+print(set(a))
