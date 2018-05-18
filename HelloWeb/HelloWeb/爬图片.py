9989b13addb20f6410f2bbcf6c2cf9847e1a0445
dorayo <yomingyo@gmail.com>
2018-05-18 17:59:03 +0800

    py

---
+++ HelloWeb/HelloWeb/爬图片.py
@@ -0,0 +1,18 @@
+import time
+from urllib import request
+from bs4 import BeautifulSoup
+import re
+
+url = r'https://www.zhihu.com/question/22918070'
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+page = request.Request(url, headers = headers)
+page_info = request.urlopen(page).read().decode('utf-8')
+soup = BeautifulSoup(page_info, 'html.parser')
+
+links = soup.find_all('img', "origin_image zh-lightbox-thumb", src = re.compile(r'.jpg$'))
+local_path = r'/Users/yangyi/Desktop/Pic'
+
+for link in links:
+    print(link.attrs['src'])
+    request.urlretrieve(link.attrs['src'], local_path + r'/%s.jpg' % time.time())
+
---
+++ HelloWeb/HelloWeb/爬文本存本地txt.py
@@ -0,0 +1,20 @@
+from urllib import request
+from bs4 import BeautifulSoup
+
+url = r'http://www.jianshu.com'
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+page = request.Request(url, headers = headers)
+page_info = request.urlopen(page).read().decode('utf-8')
+soup = BeautifulSoup(page_info, 'html.parser')
+titles = soup.find_all('a', 'title')
+# try:
+#     file = open(r'/Users/yangyi/Desktop/titles.txt', 'w')
+#     for title in titles:
+#         file.write(title.string + '\n')
+# finally:
+#     if file:
+#         file.close()
+
+with open(r'/Users/yangyi/Desktop/titles.txt', 'w') as file:
+    for title in titles:
+        file.write(title.string + '\n')
