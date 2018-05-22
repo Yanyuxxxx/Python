56ab6eedafc28b17c5af03ac8acbe27f3731fc1a
Yanyuxxxx <812363941@qq.com>
2018-05-23 00:30:54 +0800

    1111

--- HelloWeb/HelloWeb/爬图片.py
+++ HelloWeb/HelloWeb/爬图片.py
@@ -2,7 +2,7 @@ import time
 from urllib import request
 from bs4 import BeautifulSoup
 import re
-
+#www
 url = r'https://www.zhihu.com/question/22918070'
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
 page = request.Request(url, headers = headers)
