877f1e9e688e9bf9e70f79252f070042b1d43256
dorayo <yomingyo@gmail.com>
2018-05-25 14:26:40 +0800

    编程实践

---
+++ Python编程从入门到实践/9.1 类.py
@@ -0,0 +1,21 @@
+class Dog():
+    type = 10
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(self.name.title() + " is now sitting.")
+
+    def roll_over(self):
+        print(self.name.title() + " rolled over!")
+
+
+my_dog = Dog('willie', 6)
+print(my_dog.type)
+print("My dog's name is " + my_dog.name.title() + ".")
+print("My dog is " + str(my_dog.age) + " years old.")
+
+my_dog.sit()
+my_dog.roll_over()
