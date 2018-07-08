57c9e20aeb3890f6d14a8ac0700937e475820929
yuyanyu <yuyanyu@haodf.com>
2018-07-08 17:20:24 +0800

    333

--- HelloWeb/HelloWeb/爬虫.py
+++ HelloWeb/HelloWeb/爬虫.py
@@ -24,7 +24,7 @@ from urllib import request
 from bs4 import BeautifulSoup
 
 url = r'http://www.jianshu.com'
-# 模拟真实浏览器进行访问111
+# 模拟真实浏览器进行访问111333
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
 page = request.Request(url, headers=headers)
 page_info = request.urlopen(page).read()
